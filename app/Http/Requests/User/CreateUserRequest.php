f6122897d942180e73a8143e94169f992d2ec755
TheNobleBrain <burakaliildir@gmail.com>
2020-03-29 20:24:38 +0300

    Initial commit

--- app/Http/Requests/User/CreateUserRequest.php
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -9,14 +9,14 @@ class CreateUserRequest extends FormRequest
   public function rules()
   {
     return [
-      'name' => '',
-      'surname' => '',
+      'name' => 'nullable',
+      'surname' => 'nullable',
       'email' => 'unique:users|email|required',
-      'password' => '',
-      'bio' => '',
-      'phone' => 'phone:AUTO,TR', // ISO 3166-1 alpha-2 code country
+      'password' => 'nullable',
+      'bio' => 'nullable',
+      'phone' => 'nullable|phone:AUTO,TR', // ISO 3166-1 alpha-2 code country
       'gender' => 'string',
-      'date_of_birth' => 'string',
+      'date_of_birth' => 'nullable',
       'confirm' => '',
       'profile' => 'mimes:jpeg,jpg,png',
     ];
@@ -33,7 +33,8 @@ class CreateUserRequest extends FormRequest
       'phone' => 'Telefon',
       'gender' => 'Cinsiyet',
       'date_of_birth' => 'Doğum Tarihi',
-      'confirm' => 'Onay',
+      'confirm' => 'Hesap Durumu',
+      'profile' => 'Profil Fotoğrafı',
     ];
   }
 }
--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -10,14 +10,14 @@ class UpdateUserRequest extends FormRequest
   public function rules()
   {
     return [
-      'name' => '',
-      'surname' => '',
+      'name' => 'nullable',
+      'surname' => 'nullable',
       //'email' => 'unique:users|email|required',
       //'password' => '',
-      'bio' => '',
-      'phone' => 'phone:US,TR,GB', // ISO 3166-1 alpha-2 code country
+      'bio' => 'nullable',
+      'phone' => 'nullable|phone:US,TR,GB', // ISO 3166-1 alpha-2 code country
       'gender' => 'string',
-      'date_of_birth' => 'string',
+      'date_of_birth' => 'date',
       'confirm' => '',
       'profile' => 'mimes:jpeg,jpg,png',
     ];
@@ -34,7 +34,7 @@ class UpdateUserRequest extends FormRequest
       'phone' => 'Telefon',
       'gender' => 'Cinsiyet',
       'date_of_birth' => 'Doğum Tarihi',
-      'confirm' => 'Onay',
+      'confirm' => 'Hesap Durumu',
       'profile' => 'Profil Fotoğrafı',
     ];
   }
