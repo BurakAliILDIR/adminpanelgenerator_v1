14f395c9652c83aef4fa9b3aebc409505404aebc
TheNobleBrain <burakaliildir@gmail.com>
2020-04-26 23:11:24 +0300

    Initial commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,12 +7,11 @@ window._ = require('lodash');
  */
 
 try {
-  window.Popper = require('popper.js').default;
-  window.$ = window.jQuery = require('jquery');
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
 
-  require('bootstrap');
-} catch (e) {
-}
+    require('bootstrap');
+} catch (e) {}
 
 /**
  * Laravel arka ucumuza kolayca istek göndermemizi sağlayan axios HTTP kütüphanesini yükleyeceğiz.
