cea0e778200cb5a6e99357ab9019da66099ad5e4
TheNobleBrain <TheNobleBrain@gmail.com>
2020-05-09 16:38:51 +0300

    Initial commit

---
+++ Envoy.blade.php
@@ -0,0 +1,25 @@
+@servers(['localhost' => '127.0.0.1'])
+
+@story('deploy')
+setup
+@endstory
+
+@task('setup')
+echo "Depo çekme başladı"
+git pull origin master
+echo "Depo çekildi"
+echo "Kurulum başladı"
+composer install
+composer update
+
+php artisan key:generate
+php artisan build
+php artisan storage:link
+echo "Cache temizleniyor"
+php artisan config:clear
+php artisan cache:clear
+composer dump-autoload
+php artisan view:clear
+php artisan route:clear
+echo "Kurulum tamamlandı!"
+@endtask
